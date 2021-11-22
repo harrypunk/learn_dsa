be3a395defb52ea717e0b39112a11da6cdccc84e
mu <allthatjazz@outlook.com>
2021-11-22 22:49:23 +0800

    two sum with panic

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -1,23 +1,16 @@
 use std::collections::HashMap;
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut left = 0;
-    let mut right = 0;
-
     let mut map: HashMap<i32, usize> = HashMap::new();
-    for i in 0..nums.len() {
-        let current = nums[i];
-        if let Some(j) = map.get(&current) {
-            left = *j;
-            right = i;
-            break;
-        } else {
-            let diff = target - current;
-            map.insert(diff, i);
+
+    for (index, &value) in nums.iter().enumerate() {
+        if let Some(&i) = map.get(&(target - value)) {
+            return vec![i as i32, index as i32];
         }
+        map.insert(value, index);
     }
 
-    vec![left as i32, right as i32]
+    panic!()
 }
 
 #[test]
@@ -39,4 +32,11 @@ fn two_sum_exm3() {
     let n1 = vec![3, 3];
     let res = two_sum(n1, 6);
     assert_eq!(res, [0, 1]);
+}
+
+#[test]
+#[should_panic]
+fn two_sum_exm4() {
+    let n1 = vec![3, 3, 1, 12];
+    let _ = two_sum(n1, 7);
 }

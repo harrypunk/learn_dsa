007cd1caec56580a9a3c4d71b7be6b4a718553af
hp <allthatjazz@outlook.com>
2022-01-17 22:32:47 +0800

    Better iteration of add two nums

--- src/add_two_numbers.rs
+++ src/add_two_numbers.rs
@@ -19,30 +19,35 @@ impl Solution {
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        match (l1, l2) {
-            (None, None) => None,
-            (None, x) | (x, None) => x,
-            (Some(x), Some(y)) => {
-                let (num1, carry1) = add(x.val,y.val, 0);
-                let mut carry = carry1;
-                let mut head: Box<ListNode> = Box::new(ListNode::new(num1));
-                let mut tail = &head;
-                let mut left = &x.next;
-                let mut right = &y.next;
-                'calc: loop {
-                    match (left, right) {
-                        (None, None) => break 'calc,
-                        (None, )
-                    }
+        let mut result = None;
+
+        let mut tail = &mut result;
+        // (list1, list2, sum, carry)
+        let mut temp = (l1, l2, 0, 0);
+        loop {
+            temp = match temp {
+                (None, None, _, 0) => break,
+                (None, None, _, _) => (None, None, 1, 0),
+                (Some(node), None, _, carry) | (None, Some(node), _, carry) => {
+                    let (sum1, carray1) = add(node.val, 0, carry);
+                    (node.next, None, sum1, carray1)
                 }
-                Some(head)
-            }
+                (Some(n1), Some(n2), _, carry) => {
+                    let (sum1, carray1) = add(n1.val, n2.val, carry);
+                    (n1.next, n2.next, sum1, carray1)
+                }
+            };
+            *tail = Some(Box::new(ListNode::new(temp.2)));
+            tail = &mut tail.as_mut().unwrap().next;
         }
+
+        result
     }
 }
 
 //add two digits (num, carry)
-fn add(a:i32, b:i32, carry: i32) -> (i32, i32) {
-    let sum = a+b+carry;
-    (sum%10, sum/10)
+fn add(a: i32, b: i32, carry: i32) -> (i32, i32) {
+    let sum = a + b + carry;
+    let carry1 = if sum > 9 { 1 } else { 0 };
+    (sum - carry1 * 10, carry1)
 }
